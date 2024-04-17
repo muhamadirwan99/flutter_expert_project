637b89ee698c3efb8dee57d7b9ddf6d82370b074
Muhamad Irwan <muhamadirwan99@gmail.com>
2024-04-17 08:32:33 +0700

    feat(tvseries): show list top rated tv series

--- lib/injection.dart
+++ lib/injection.dart
@@ -25,6 +25,7 @@ import 'package:ditonton/presentation/provider/movie_search_notifier.dart';
 import 'package:ditonton/presentation/provider/popular_movies_notifier.dart';
 import 'package:ditonton/presentation/provider/top_rated_movies_notifier.dart';
 import 'package:ditonton/presentation/provider/tv_series/popular_tv_series_notifier.dart';
+import 'package:ditonton/presentation/provider/tv_series/top_rated_tv_series_notifier.dart';
 import 'package:ditonton/presentation/provider/tv_series/tv_series_list_notifier.dart';
 import 'package:ditonton/presentation/provider/watchlist_movie_notifier.dart';
 import 'package:http/http.dart' as http;
@@ -98,11 +99,11 @@ void init() {
       locator(),
     ),
   );
-  // locator.registerFactory(
-  //   () => TopRatedMoviesNotifier(
-  //     getTopRatedMovies: locator(),
-  //   ),
-  // );
+  locator.registerFactory(
+    () => TopRatedTvSeriesNotifier(
+      getTopRatedTvSeries: locator(),
+    ),
+  );
   // locator.registerFactory(
   //   () => WatchlistMovieNotifier(
   //     getWatchlistMovies: locator(),
