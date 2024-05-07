18097aa9e6d0da4fcd1883226e86c96a398e8897
Muhamad Irwan <muhamadirwan99@gmail.com>
2024-05-07 09:33:00 +0700

    change json reader add dir packages

--- packages/core/test/json_reader.dart
+++ packages/core/test/json_reader.dart
@@ -2,11 +2,8 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('/packages/core/test')) {
-    dir = dir.replaceAll('/packages/core/test', '');
+  if (dir.endsWith('packages/core/test/')) {
+    dir = dir.replaceAll('packages/core/test/', '');
   }
-  if (dir.endsWith('tv')) {
-    return File('$dir/packages/core/test/$name').readAsStringSync();
-  }
-  return File('$dir/packages/tv/test/$name').readAsStringSync();
+  return File('$dir/packages/core/test/$name').readAsStringSync();
 }
